93b49af1b393af2aaed2d85b270772efa6fd55a8
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2026-02-28 18:31:27 -0600

    fixed minor bug

--- src/main/java/com/williamcherres/notes_api/config/CorsConfig.java
+++ src/main/java/com/williamcherres/notes_api/config/CorsConfig.java
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://notes-web-1.vercel.app")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "https://notes-web-1.vercel.app",
+                        "http://localhost:5173"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
